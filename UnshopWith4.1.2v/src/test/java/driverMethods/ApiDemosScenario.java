87ee6ea52db9beefd163bb53b8297b8336195901
githubforTejas <105335742+githubforTejas@users.noreply.github.com>
2022-07-07 10:22:54 +0530

    Update ApiDemosScenario.java

--- UnshopWith4.1.2v/src/test/java/driverMethods/ApiDemosScenario.java
+++ UnshopWith4.1.2v/src/test/java/driverMethods/ApiDemosScenario.java
@@ -14,6 +14,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class ApiDemosScenario {
+	//Hii hello
 	@Test
 	public void scenario() throws MalformedURLException
 	{
